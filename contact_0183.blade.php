86219629f711bd7f4c25d37d91817a5f70fdb221
Muhammad Fahmi Izhar 1461800183 <78761531+Izhar112@users.noreply.github.com>
2021-05-22 13:20:22 +0700

    Add files via upload

---
+++ contact_0183.blade.php
@@ -0,0 +1,19 @@
+
+<div class="container">
+    <div class="col s15">
+    <div class="card-panel default">
+    <div class="row">
+
+        <div class="col s4">  
+                <h2>
+                    Nama      : Muhammad Fahmi Izhar <br>
+                    NBI       : 1461800183<br>
+                    Email     : fahmibonek30@gmail.com<br>
+                    Instagram : fahmiizhar_
+                </h2>
+                <br>
+        </div>
+        </div>   
+    </div>   
+    </div>
+</div>
---
+++ home_0183.blade.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        
+    <div class="row">
+        <div class="col s15">
+            <div class="card-panel default">
+                <center>
+                    <h1>SELAMAT DATANG</h1>
+                    <h1>Tugas Praktikum 1</h1>
+                    <h1>Muhammad fahmi Izhar 1461800183</h1>
+                    
+                </center>
+                
+            </div>
+        </div>
+        
+    </div>
+</div>
